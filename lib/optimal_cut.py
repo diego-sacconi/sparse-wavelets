4d3bbc62c3cac522082c37680dd4ae77d0801b84
diego-sacconi <diego-sacconi@users.noreply.github.com>
2017-08-28 12:54:25 +0200

    Remove beta from sweep_opt arguments

--- lib/optimal_cut.py
+++ lib/optimal_cut.py
@@ -19,18 +19,17 @@ def sqrtm(mat):
 		* matrix square root
 	"""
 	eigvals, eigvecs = eigh(mat)
-	
+
 	eigvecs = eigvecs[:, eigvals > 0]
 	eigvals = eigvals[eigvals > 0]
-	
+
 	return dot(eigvecs, dot(diag(sqrt(eigvals)), eigvecs.T))
 
-def sweep_opt(x, beta, F, G, k, ind):
+def sweep_opt(x, F, G, k, ind):
 	"""
 		Sweep algorithm for sparse wavelets.
 		Input:
 			* x: continuous indicator vector
-			* beta: beta parameter for regularization
 			* F: graphsignal
 			* G: graph
 			* k: max number of edges to be cut
@@ -47,11 +46,11 @@ def sweep_opt(x, beta, F, G, k, ind):
 	size_one = 0
 	sum_one = 0
 	sum_two = 0
-	
+
 	for v in G.nodes():
 		sum_two = sum_two + F[ind[v]]
-	
-	edges_cut = 0 
+
+	edges_cut = 0
 	nodes_one = {}
 	total_size = networkx.number_of_nodes(G)
 
@@ -59,39 +58,39 @@ def sweep_opt(x, beta, F, G, k, ind):
 		size_one = size_one + 1
 		sum_one = sum_one + F[ind[G.nodes()[sorted_x[i]]]]
 		sum_two = sum_two - F[ind[G.nodes()[sorted_x[i]]]]
-		
+
 		nodes_one[G.nodes()[sorted_x[i]]] = True
-		
+
 		for v in G.neighbors(G.nodes()[sorted_x[i]]):
 			if v not in nodes_one:
 				edges_cut = edges_cut + 1
 			else:
 				edges_cut = edges_cut - 1
-		
+
 		den = size_one  * (total_size-size_one) * total_size
 		if den > 0:
 			val = math.pow(sum_one*(total_size-size_one) - sum_two*size_one, 2) / den
 		else:
 			val = 0
-		
+
 		if val >= best_val and edges_cut <= k:
 			best_cand = i
 			best_val = val
 			best_edges_cut = edges_cut
-			
+
 			if total_size * size_one * (total_size-size_one) > 0:
 				energy = math.pow(sum_one*(total_size-size_one) - sum_two*size_one, 2) / (total_size * size_one * (total_size-size_one))
 			else:
 				energy = 0
 
 	vec = numpy.zeros(total_size)
-	
+
 	for i in range(x.shape[0]):
 		if i <= best_cand:
 			vec[sorted_x[i]] = -1.
 		else:
 			vec[sorted_x[i]] = 1.
-	
+
 	return vec, best_val, best_edges_cut, energy
 
 def laplacian_complete(n):
@@ -106,7 +105,7 @@ def laplacian_complete(n):
 	C = -1 * C
 	D = numpy.diag(numpy.ones(n))
 	C = (n)*D + C
-	
+
 	return C
 
 def weighted_adjacency_complete(G, F, ind):
@@ -124,7 +123,7 @@ def weighted_adjacency_complete(G, F, ind):
 		A.append([])
 		for u in G.nodes():
 			A[-1].append(pow(F[ind[v]]-F[ind[u]],2))
-			
+
 	return numpy.array(A)
 
 def fast_cac(G, F, ind):
@@ -134,7 +133,7 @@ def fast_cac(G, F, ind):
 			* G: graph
 			* F: graph signal
 			* ind: vertex index v: unique integer
-		Output: 
+		Output:
 			* CAC: matrix product
 	"""
 	CAC = []
@@ -188,18 +187,18 @@ def spectral_cut(CAC, L, C, A, start, F, G, beta, k, ind):
 	"""
 	isqrtCL = sqrtmi( C + beta * L)
 	M = numpy.dot(numpy.dot(isqrtCL, CAC), isqrtCL)
-	
+
 	(eigvals, eigvecs) = scipy.linalg.eigh(M,eigvals=(0,0))
 	x = numpy.asarray(numpy.dot(eigvecs[:,0], isqrtCL))[0,:]
 
-	(x, score, size, energy) = sweep_opt(x, beta, F, G, k, ind)
-	
+	(x, score, size, energy) = sweep_opt(x, F, G, k, ind)
+
 	res = {}
 	res["x"] = numpy.array(x)
 	res["size"] = size
 	res["score"] = score
 	res["energy"] = energy
-	
+
 	return res
 
 def eig_vis_opt(G, F, beta):
@@ -211,23 +210,23 @@ def eig_vis_opt(G, F, beta):
 			* beta: regularization parameter
 		Output:
 			* v1: first eigenvector
-			* v2: second eigenvector	
+			* v2: second eigenvector
 	"""
 	ind = {}
 	i = 0
-	
+
 	for v in G.nodes():
 		ind[v] = i
 		i = i + 1
-	
+
 	C = laplacian_complete(networkx.number_of_nodes(G))
 	A = weighted_adjacency_complete(G, F, ind)
 	CAC = numpy.dot(numpy.dot(C,A), C)
 	L = networkx.laplacian_matrix(G).todense()
-	
+
 	isqrtCL = sqrtmi( C + beta * L)
 	M = numpy.dot(numpy.dot(isqrtCL, CAC), isqrtCL)
-	
+
 	(eigvals, eigvecs) = scipy.linalg.eigh(M,eigvals=(0,1))
 	x1 = numpy.asarray(numpy.dot(eigvecs[:,0], isqrtCL))[0,:]
 	x2 = numpy.asarray(numpy.dot(eigvecs[:,1], isqrtCL))[0,:]
@@ -244,8 +243,8 @@ def trans(L, min_v, max_v):
 		Output:
 			* translation
 	"""
-	return (float(2.) / (max_v-min_v)) * L, -(float(max_v+min_v) / (max_v-min_v)) 
-    
+	return (float(2.) / (max_v-min_v)) * L, -(float(max_v+min_v) / (max_v-min_v))
+
 def fun(k, n, beta, min_v, max_v, x):
 	"""
 		Function to be integrated in Chebyshev polynomial computation.
@@ -258,9 +257,9 @@ def fun(k, n, beta, min_v, max_v, x):
 			* function value
 	"""
 	y = 0.5 * math.cos(x) * float(max_v - min_v) +  (0.5 * (max_v + min_v))
-	
+
 	return math.cos(k*x)*(float(1.) / math.sqrt(beta*y))
-		    
+
 def coef(k, n, beta, min_v, max_v):
 	"""
 		Chebyshev polynomial coefficients.
@@ -273,7 +272,7 @@ def coef(k, n, beta, min_v, max_v):
 			* coefficient
 	"""
 	return float(2. * scipy.integrate.quad(lambda x: fun(k, n, beta, min_v, max_v, x), 0., math.pi)[0]) / math.pi
-			
+
 def chebyshev_approx_2d(n, beta, X, L):
 	"""
 		Approximates sqrt((L)^+)^T * X * sqrt((L)^+)^T using Chebyshev polynomials (twice)
@@ -287,28 +286,28 @@ def chebyshev_approx_2d(n, beta, X, L):
 	"""
 	max_v = beta * L.shape[0]
 	min_v = 1
-	
+
 	ts1, ts2 = trans(L, min_v, max_v)
 	P1 = 0.5 * coef(0, L.shape[0], beta, min_v, max_v) * X
-	tkm2 = X 
+	tkm2 = X
 	tkm1 = scipy.sparse.csr_matrix.dot(ts1, X) + ts2 * X
 	P1 = P1 + coef(1, L.shape[0], beta, min_v, max_v) * tkm1
-	
-	for i in range(2, n):                 
-		Tk = 2. * (scipy.sparse.csr_matrix.dot(ts1, tkm1) + ts2 * tkm1) - tkm2  
+
+	for i in range(2, n):
+		Tk = 2. * (scipy.sparse.csr_matrix.dot(ts1, tkm1) + ts2 * tkm1) - tkm2
 		P1 = P1 + coef(i, L.shape[0], beta, min_v, max_v) * Tk
 		tkm2 = tkm1
 		tkm1 = Tk
-	
-	P1 = P1.transpose()    
+
+	P1 = P1.transpose()
 	P2 = 0.5 * coef(0, L.shape[0], beta, min_v, max_v) * P1
-	tkm2 = P1 
+	tkm2 = P1
 	tkm1 = scipy.sparse.csr_matrix.dot(ts1, P1) + ts2 * P1
 
 	P2 = P2 + coef(1, L.shape[0], beta, min_v, max_v) * tkm1
-										        
-	for i in range(2, n):                 
-		Tk = 2. * (scipy.sparse.csr_matrix.dot(ts1, tkm1) + ts2 * tkm1) - tkm2  
+
+	for i in range(2, n):
+		Tk = 2. * (scipy.sparse.csr_matrix.dot(ts1, tkm1) + ts2 * tkm1) - tkm2
 		P2 = P2 + coef(i, L.shape[0], beta, min_v, max_v) * Tk
 		tkm2 = tkm1
 		tkm1 = Tk
@@ -323,25 +322,25 @@ def chebyshev_approx_1d(n, beta, x, L):
 			* beta: regularization parameter
 			* x: vector
 			* L: graph Laplacian
-		Output: 	
+		Output:
 			* P: approximation
-			
+
 	"""
 	max_v = beta * L.shape[0]
 	min_v = 1
-	
+
 	ts1, ts2 = trans(L, min_v, max_v)
 	P = 0.5 * coef(0, L.shape[0], beta, min_v, max_v) * x
 	tkm2 = x
 	tkm1 = scipy.sparse.csr_matrix.dot(ts1, x) + ts2 * x
 	P = P + coef(1, L.shape[0], beta, min_v, max_v) * tkm1
-	
-	for i in range(2, n):                 
-		Tk = 2. * (scipy.sparse.csr_matrix.dot(ts1, tkm1) + ts2 * tkm1) - tkm2  
+
+	for i in range(2, n):
+		Tk = 2. * (scipy.sparse.csr_matrix.dot(ts1, tkm1) + ts2 * tkm1) - tkm2
 		P = P + coef(i, L.shape[0], beta, min_v, max_v) * Tk
 		tkm2 = tkm1
 		tkm1 = Tk
-	
+
 	return P
 
 def cheb_spectral_cut(CAC, start, F, G, beta, k, n, ind):
@@ -366,18 +365,18 @@ def cheb_spectral_cut(CAC, start, F, G, beta, k, n, ind):
 	"""
 	L = networkx.laplacian_matrix(G)
 	M = chebyshev_approx_2d(n, beta, CAC, L)
-	
+
 	eigvec = power_method(-M, start, 10)
 	x = chebyshev_approx_1d(n, beta, eigvec, L)
-	
-	(x, score, size, energy) = sweep_opt(x, beta, F, G, k, ind)
-	
+
+	(x, score, size, energy) = sweep_opt(x, F, G, k, ind)
+
 	res = {}
 	res["x"] = numpy.array(x)
 	res["size"] = size
 	res["score"] = score
 	res["energy"] = energy
-	
+
 	return res
 
 def fast_search(G, F, k, n, ind):
@@ -424,20 +423,20 @@ def one_d_search(G, F, k, ind):
 	b = 1000.
 	c=b-gr*(b-a)
 	d=a+gr*(b-a)
-	
+
 	#Tolerance
 	tol = 1.
 
 	resab = {}
 	resab["size"] = k + 1
-	
+
 	#golden search
-	while abs(c-d)>tol or resab["size"] > k:      
+	while abs(c-d)>tol or resab["size"] > k:
 		resc = spectral_cut(CAC, L, C, A, start, F, G, c, k, ind)
 		resd = spectral_cut(CAC, L, C, A, start, F, G, d, k, ind)
-		
-		if resc["size"] <= k: 
-			if resc["score"] > resd["score"]: 
+
+		if resc["size"] <= k:
+			if resc["score"] > resd["score"]:
 				start = numpy.array(resc["x"])
 				b = d
 				d = c
@@ -445,16 +444,16 @@ def one_d_search(G, F, k, ind):
 			else:
 				start = numpy.array(resd["x"])
 				a=c
-				c=d  
+				c=d
 				d=a+gr*(b-a)
 		else:
 				start = numpy.array(resc["x"])
 				a=c
-				c=d  
+				c=d
 				d=a+gr*(b-a)
-		
+
 		resab = spectral_cut(CAC, L, C, A, start, F, G, (b+a) / 2, k, ind)
-	
+
 	return resab
 
 def get_subgraphs(G, cut):
@@ -481,7 +480,7 @@ def get_subgraphs(G, cut):
 
 	G1 = G.subgraph(P1)
 	G2 = G.subgraph(P2)
-	
+
 	return G1, G2
 
 def optimal_wavelet_basis(G, F, k, npol):
@@ -497,7 +496,7 @@ def optimal_wavelet_basis(G, F, k, npol):
 			* ind: vertex index vertex: unique integer
 			* size: number of edges cut
 	"""
-	
+
 	#Creating index
 	ind = {}
 	i = 0
@@ -517,14 +516,14 @@ def optimal_wavelet_basis(G, F, k, npol):
 
 	c["parent"] = root
 	c["graph"] = G
-	
+
 	cand_cuts.append(c)
 
 	#Recursively compute new cuts
 	while size <= k and len(cand_cuts) > 0:
 		best_cut = None
 		b = 0
-		
+
 		for i in range(0, len(cand_cuts)):
 			if cand_cuts[i]["size"] + size <= k and cand_cuts[i]["score"] > 0:
 				if best_cut is None or cand_cuts[i]["score"] > best_cut["score"]:
@@ -543,24 +542,24 @@ def optimal_wavelet_basis(G, F, k, npol):
 				best_cut["parent"].add_child(n)
 			elif networkx.number_of_nodes(G1) > 0:
 				n = Node(None)
-				
+
 				if npol == 0:
 					c = one_d_search(G1, F, k, ind)
 				else:
 					c = fast_search(G1, F, k, npol, ind)
-				
+
 				c["parent"] = n
 				c["graph"] = G1
 				cand_cuts.append(c)
 
 				best_cut["parent"].add_child(n)
-			
+
 			if networkx.number_of_nodes(G2) == 1:
 				n = Node(ind[G2.nodes()[0]])
 				best_cut["parent"].add_child(n)
 			elif networkx.number_of_nodes(G2) > 0:
 				n = Node(None)
-				
+
 				if npol == 0:
 					c = one_d_search(G2, F, k, ind)
 				else:
@@ -569,17 +568,15 @@ def optimal_wavelet_basis(G, F, k, npol):
 				c["parent"] = n
 				c["graph"] = G2
 				cand_cuts.append(c)
-				
+
 				best_cut["parent"].add_child(n)
-			
+
 			del cand_cuts[b]
-	
+
 	#Compute remaining cuts using ratio cuts once budget is over (not optimal)
 	for i in range(0, len(cand_cuts)):
 		rc_recursive(cand_cuts[i]["parent"], cand_cuts[i]["graph"], ind)
 
 	set_counts(root)
-	
+
 	return root, ind, size
-
-
