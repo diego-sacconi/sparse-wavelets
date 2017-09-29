4d6bc5fc5b97c690b02f7193a43b3f30975cdd30
diego-sacconi <diego-sacconi@users.noreply.github.com>
2017-09-29 13:03:47 +0200

    Update lib/io_utils.py

--- lib/io_utils.py
+++ lib/io_utils.py
@@ -1,11 +1,9 @@
 """
 This module read graph's info from files with the following format:
-
 input_graph_name has info about edges.
-Row format: "vertex_A, vertex_B[, edge_weight]"
-
+Row format: "node_A, node_B[, edge_weight]"
 input_data_name has info about the graph signal.
-Row format : "vertex_id, vertex_value"
+Row format : "node_id, node_value"
 """
 
 import networkx as nx
@@ -17,24 +15,22 @@ def read_graph(input_graph_name, input_data_name):
         Read graph from file.
         Input:
             * input_graph_name has info about edges.
-                Row format: "vertex_A, vertex_B[, edge_weight]"
+                Row format: "node_A, node_B[, edge_weight]"
             * input_data_name has info about the graph signal.
-                Row format : "vertex_id, vertex_value"
+                Row format : "node_id, node_value"
         Output:
             * networkx graph
     """
 
     # Reading input data
     input_data = open(input_data_name, 'r')
-    values = {}
+    graph_signal = {}
 
     for line in input_data:
         line = line.rstrip()
-        vec = line.rsplit(',')
-
-        vertex = vec[0]
-        value = float(vec[1])
-        values[vertex] = value
+        node, value = line.rsplit(',')
+        value = float(value)
+        graph_signal[node] = value
 
     input_data.close()
 
@@ -44,32 +40,24 @@ def read_graph(input_graph_name, input_data_name):
 
     for line in input_graph:
         line = line.rstrip()
-        vec = line.rsplit(',')
-        v1 = vec[0]
-        v2 = vec[1]
+        node_A, node_B = line.rsplit(',')[:2]
         # Note that the edge weight is always set to 1
         # even when provided available in input_graph_name
-        if v1 in values and v2 in values:
-            G.add_edge(v1, v2, weight=1.)
+        if node_A in graph_signal and node_B in graph_signal:
+            G.add_edge(node_A, node_B, weight=1.)
+
+    input_graph.close()
 
     # Extracting largest connected component from graph
     Gcc = sorted(nx.connected_component_subgraphs(G), key=len, reverse=True)
 
     G = Gcc[0]
 
-    graph_signal = {}
-
     # Setting the graph_signal as node attribute
-    for v in values.keys():
-        if v in G:
-            graph_signal[v] = values[v]
+    for node, value in graph_signal.items():
+        if node in G:
+            G.node[node]["value"] = value
 
-<<<<<<< Updated upstream:lib/io.py
-    input_graph.close()
-    nx.set_node_attributes(G, "value", graph_signal)
-
-=======
->>>>>>> Stashed changes:lib/io_utils.py
     return G
 
 
@@ -78,7 +66,7 @@ def read_values(input_data_name, G):
         Read the graph signal from file
         Input:
             * input_data_name has info about the graph signal.
-                Row format : "vertex_id, vertex_value"
+                Row format : "node_id, node_value"
             * G: networkx graph
         Output:
             * F: normalized node values array, ordered by G.nodes()
@@ -89,18 +77,15 @@ def read_values(input_data_name, G):
     # Reading file
     for line in input_data:
         line = line.rstrip()
-        vec = line.rsplit(',')
-
-        vertex = vec[0]
-        value = float(vec[1])
-        graph_signal[vertex] = value
+        node, value = line.rsplit(',')
+        graph_signal[node] = float(value)
 
     input_data.close()
 
     F = []
-    for v in G.nodes():
-        if v in graph_signal:
-            F.append(float(graph_signal[v]))
+    for node in G.nodes():
+        if node in graph_signal:
+            F.append(graph_signal[node])
         else:
             F.append(0.)
 
